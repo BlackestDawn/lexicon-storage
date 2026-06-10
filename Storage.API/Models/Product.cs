e94c218bdad2006f5d54c6ebcc261f67a1c4b5ea
Alexander Stauch <blacke4dawn@gmail.com>
2026-06-10 17:20:16 +0200

    adding forgotten default value to Product model

--- Storage.API/Models/Product.cs
+++ Storage.API/Models/Product.cs
@@ -14,7 +14,7 @@ public class Product(string name)
   public decimal Price { get; set; }
   [Required]
   [StringLength(40, MinimumLength = 3)]
-  public string Category { get; set; }
+  public string Category { get; set; } = string.Empty;
   [StringLength(10, MinimumLength = 3)]
   public string? Shelf { get; set; }
   public int Count { get; set; }
