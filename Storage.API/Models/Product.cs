b01050640ca3d63a7c7622737bb5e67c766546bc
Alexander Stauch <blacke4dawn@gmail.com>
2026-06-10 14:22:17 +0200

    adding annotations to product model

--- Storage.API/Models/Product.cs
+++ Storage.API/Models/Product.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Storage.API.Models;
 
-public class Product(int id, string name, decimal price, string category, string shelf, int count, string? description)
+public class Product(int id, string name, decimal price, string category, string? shelf, int count, string? description)
 {
+  [Key]
+  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
   public int Id { get; set; } = id;
+  [StringLength(50, MinimumLength = 3)]
   public string Name { get; set; } = name;
   public decimal Price { get; set; } = price;
+  [StringLength(40, MinimumLength = 3)]
   public string Category { get; set; } = category;
-  public string Shelf { get; set; } = shelf;
+  [StringLength(10, MinimumLength = 3)]
+  public string? Shelf { get; set; } = shelf;
   public int Count { get; set; } = count;
+  [StringLength(200, MinimumLength = 3)]
   public string? Description { get; set; } = description;
 }
